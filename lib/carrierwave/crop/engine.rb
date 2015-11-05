3a91399a45428631d61ff81ea988e17bcdd74e06
Jeremy Fleischman <jeremyfleischman@gmail.com>
2015-11-04 22:33:31 -0800

    Workaround for https://github.com/kirtithorat/carrierwave-crop/issues/17, inspired by https://github.com/twbs/bootstrap-sass/blob/master/lib/bootstrap-sass/engine.rb.

--- lib/carrierwave/crop/engine.rb
+++ lib/carrierwave/crop/engine.rb
@@ -3,6 +3,12 @@
 module CarrierWave
   module Crop
     class Engine < ::Rails::Engine
+      initializer 'bootstrap-sass.assets.precompile' do |app|
+        %w(stylesheets javascripts images).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
+        app.config.assets.precompile += %w( Jcrop.gif )
+	  end
     end
   end
 end
